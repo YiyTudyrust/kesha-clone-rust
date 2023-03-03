72af634cc832b9817af5a7ed6c1b66da4e8b2b32
YiyTudyrust <126882847+YiyTudyrust@users.noreply.github.com>
2023-03-03 20:28:23 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     println!("Hay, welcome to {name_of_app}.This app can repeat your answer!");
     println!("Please input anythings.");
     let mut anything = String::new();
-    io::stdin().read_line(&mut anything).expect("Failed in read line!");
+    io::stdin().read_line(&mut anything).expect("Failed to read line!");
     println!("{name_of_app} say: {anything}");
 }
