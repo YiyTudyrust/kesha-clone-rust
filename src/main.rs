14bb363fcf4a4666c89124c79790942ac1a380bf
YiyTudyrust <redtfoqmead@proton.me>
2023-03-03 18:53:25 +0500

    first version

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+use std::io::stdin;
+
+fn main() {
+    let name_of_app:&str = "Kesha-clone-rust";
+    println!("Hay, welcome to {name_of_app}.This app can repeat your answer!");
+    println!("Please input anythings.");
+    let mut anything = String::new();
+    io::stdin().read_line(&mut anything).expect("Failed in read line!");
+    println!("{name_of_app} say: {anything}");
+}
